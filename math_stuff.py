f75fa16ba818a26d6c7933f3b7dcb2bade3ff850
egalvao-stack <eduardo.galvao@mcvts.org>
2024-11-05 09:20:20 -0500

    Streted with the math_stuff

---
+++ math_stuff.py
@@ -0,0 +1,19 @@
+import math
+
+pt=float(10.0)
+gt=float(9.0)
+losses=19.43
+nr=0.55
+greek_11=0.136363636
+kb=-228.6
+ts=float(22)
+DR=float(input("Enter DR "))
+R=float(input("Enter R "))
+
+
+
+def calculations(DR, R):
+    print("placeholder")
+    float((10(pt+gt-losses+10(math.log10(nr(((math.pi)+DR/greek_11)**2)))-20(math.log10(4000(math.pi)*R/greek_11))-kb-10(math.log10(ts)))/10)/1000)
+
+calculations(DR, R)
