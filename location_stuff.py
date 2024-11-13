5f9cf6fa4afe038da8d34ac6724dc4c916e3f8d1
Bryce-L2 <bryce.lombardo@mcvts.org>
2024-11-13 10:44:10 -0500

    Update location_stuff.py

--- location_stuff.py
+++ location_stuff.py
@@ -1,2 +1,22 @@
-f = open("data.csv", "r")
-print(f.read())
+import csv
+
+#f = open("nasaadc-main/data.csv", "r")
+
+with open("nasaadc-main/data.csv", newline='') as f:
+    reader = csv.reader(f)
+    data = list(reader)
+for x in range(1, 10):
+    print(data[x][1])
+    # for y in range(1, 10):
+    #     print(data[y][1])
+    #     for z in range(1, 10):
+    #         print(data[z][1])
+# Takes data from csv and converts to array
+#data = f.read().split("\n")
+#print(data)
+
+# df = open("nasaadc-main/data.txt", "w")
+# df.write(str(data))
+# df.close()
+
+# print(f.read().split())
