7ce97bd7aa70bdf1cb8ed285837453e149657377
Bryce-L2 <bryce.lombardo@mcvts.org>
2024-11-15 09:15:52 -0500

    Update location_stuff.py

--- location_stuff.py
+++ location_stuff.py
@@ -2,15 +2,15 @@ import csv
 
 #f = open("nasaadc-main/data.csv", "r")
 
-with open("nasaadc-main/data.csv", newline='') as f:
+with open("nasaadc-main/newData.csv", newline='') as f:
     reader = csv.reader(f)
     data = list(reader)
-for x in range(1, 10):
-    print(data[x][1])
-    # for y in range(1, 10):
-    #     print(data[y][1])
-    #     for z in range(1, 10):
-    #         print(data[z][1])
+    for x in range(1, len(data)):
+        print("x: ", data[x][1])
+    for y in range(1, len(data)):
+        print("y: ", data[y][1])
+    for z in range(1, len(data)):
+        print("z: ", data[z][1])
 # Takes data from csv and converts to array
 #data = f.read().split("\n")
 #print(data)
