9993f07102453cc017825409046a3bfdde1ba032
Bryce-L2 <bryce.lombardo@mcvts.org>
2024-11-19 09:20:04 -0500

    Update data.py

--- data.py
+++ data.py
@@ -1,18 +1,29 @@
 import csv
 
-#f = open("nasaadc-main/data.csv", "r")
+orionX = []
+orionY = []
+orionZ = []
 
-with open("newData.csv", newline='') as f:
+with open("nasaadc-main/newData.csv", newline='') as f:
     reader = csv.reader(f)
     data = list(reader)
-    # Prints out the data, not needed now but good for debugging
-    #for x in range(1, len(data)):
-    #    print("x: ", data[x][1])
-    #for y in range(1, len(data)):
-    #    print("y: ", data[y][1])
-    #for z in range(1, len(data)):
-    #    print("z: ", data[z][1])
 
-# Use this method to get any value from the data when importing this file
-def getValue(x, y):
-    return data[x][y]
+    # Prints out the data, not needed now but good for debugging
+#takes the data after the header so it starts at first number
+    for line in data[1:]:
+    #gets the numbers in the first colum (the x)
+        orionX.append(line[1])
+    print(orionX)
+
+#takes the data after the header so it starts at first number
+    for line in data[1:]:
+    #gets numbers in the second colum (the y)
+        orionY.append(line[2])
+    print(orionY)
+
+#takes the data after the header so it starts at first number
+    for line in data[1:]:
+          #gets the numbersin the third colum (they z)
+        orionZ.append(line[3])
+    print(orionZ) 
+
