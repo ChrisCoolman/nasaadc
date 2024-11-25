b8d0997cecece163fdc777b2f19476a75b9ee003
MatthewMartin93 <matthew.martin@mcvts.org>
2024-11-25 10:39:08 -0500

    Update data.py

--- data.py
+++ data.py
@@ -4,6 +4,10 @@ orionX = []
 orionY = []
 orionZ = []
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> dcba7f087727da0d0d018b2805d94d1f88819003
 with open("newData.csv", newline='') as f:
     reader = csv.reader(f)
     data = list(reader)
@@ -13,17 +17,17 @@ with open("newData.csv", newline='') as f:
     for line in data[1:]:
     #gets the numbers in the first colum (the x)
         orionX.append(line[1])
-    print(orionX)
+    #print(orionX)
 
 #takes the data after the header so it starts at first number
     for line in data[1:]:
     #gets numbers in the second colum (the y)
         orionY.append(line[2])
-    print(orionY)
+    #print(orionY)
 
 #takes the data after the header so it starts at first number
     for line in data[1:]:
           #gets the numbersin the third colum (they z)
         orionZ.append(line[3])
-    print(orionZ) 
+    #print(orionZ) 
 
