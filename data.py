15eeead2c287567a3e37f9fc235b5a14f3bda7c1
egalvao-stack <eduardo.galvao@mcvts.org>
2024-12-03 19:52:29 -0500

    Continuing to work on the data code.
    Yeah I am just pushing this bc I want to keep you all up to date with the work im doing so just assume all the smaller comits are just small additions or tweeks

--- data.py
+++ data.py
@@ -4,8 +4,11 @@ orionX = []
 orionY = []
 orionZ = []
 Ds54 = []
+Ds54_data = []
 Ds24 = []
+Ds24_data = []
 Ds34 = []
+Ds34_data = []
 
 #10 is where i need to start -Eddy 
 with open("newData.csv", newline='') as f:
@@ -31,7 +34,6 @@ with open("newData.csv", newline='') as f:
         orionZ.append(line[3])
     #print(orionZ) 
 
-
     for line in data[1:]:
         #gets numbers in tenth colum (if Ds54 is avaailable)
         Ds54.append(line[10])
@@ -39,10 +41,25 @@ with open("newData.csv", newline='') as f:
 
     for line in data[1:]:
         #gets numbers in twelth colum (if Ds24 is avaailable)
-        Ds54.append(line[12])
+        Ds24.append(line[12])
     #print(Ds24)
 
     for line in data[1:]:
         #gets numbers in fourteenth colum (if Ds34 is avaailable)
         Ds34.append(line[14])
     #print(Ds34)
+
+    for line in data[1:]:
+        #gets numbers in tenth colum (Data from Ds54)
+        Ds54_data.append(line[11])
+    #print(Ds54_data)
+
+    for line in data[1:]:
+        #gets numbers in twelth colum (Data from Ds24)
+        Ds24_data.append(line[13])
+    #print(Ds24_data)
+
+    for line in data[1:]:
+        #gets numbers in fourteenth colum (Data from Ds34)
+        Ds34_data.append(line[15])
+    #print(Ds34_data)
